28631889137b70e4f0e886e07144a18b93fc12a1
hsbin316 <hsbin316@gmail.com>
2022-12-29 21:11:44 +0900

    영화 API 호출

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <div className="App">기본환경</div>;
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/list_movies.json?sort_by=year`)
+    ).json();
+    setMovies(json.data.moveis);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <div>
+      <h4>{loading ? "Loading..." : "Ready..."}</h4>
+    </div>
+  );
 }
 
 export default App;
