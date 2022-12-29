c9b0effb8c928768c5796366a197f4b0a6171bfb
hsbin316 <hsbin316@gmail.com>
2022-12-29 23:50:41 +0900

    영화 리스트 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
+import List from "./routes/List";
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/list">
+          <List />
+        </Route>
         <Route path="/movie/:id">
           <Detail />
         </Route>
