b8f8c8fe098c75ea115b47cced01c7a27379d7ce
hsbin316 <hsbin316@gmail.com>
2022-12-30 21:19:28 +0900

    리스트 퍼블리싱

--- src/routes/List.js
+++ src/routes/List.js
@@ -3,30 +3,35 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Loading from "../components/Loading";
 
+import img from "../components/asdf.jpg";
+
 function List() {
   const { list } = useParams();
   const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  ]);
   const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?&${list}&sort_by=year`
-      )
-    ).json();
-    setMovies(json.data.movies);
+    // const json = await (
+    //   await fetch(
+    //     `https://yts.mx/api/v2/list_movies.json?&${list}&sort_by=year`
+    //   )
+    // ).json();
+    // setMovies(json.data.movies);
     setLoading(false);
   };
   useEffect(() => {
     getMovies();
   }, []);
   return (
-    <div>
-      {loading ? (
-        <Loading />
-      ) : (
-        movies.map((movie) => (
-          <div key={movie.id}>
-            <img
+    <div className="container">
+      <div className="movieList">
+        {loading ? (
+          <Loading />
+        ) : (
+          movies.map((movie) => (
+            <div className="movieSummary" key={movie.id}>
+              {/* <img
               src={movie.medium_cover_image}
               alt={movie.title}
               title={movie.title}
@@ -34,10 +39,30 @@ function List() {
             <h2>
               <Link to={`/movie/${movie.id}`}>{movie.title}</Link>
             </h2>
-            <p>{movie.summary}</p>
-          </div>
-        ))
-      )}
+            <p>{movie.summary}</p> */}
+              <img
+                className="movieImage"
+                src={img}
+                alt={"Asdf"}
+                title={"Asdfas"}
+              />
+              <div>
+                <h2 className="movieTitle">
+                  <Link to={`/movie/${movie.id}`}>{"asasdfdf"}</Link>
+                </h2>
+                <span className="movieYear">{"2022"}</span>
+                <div className="movieText">
+                  <p>
+                    {
+                      "asdfasdfasdfasdfasdbfjkabdfkjbajksdfblkajsdbfjkabdfjkabsdfjkbakjsdfbakjsbdfkjabsdfjkbajksdfbjakbdsfajkdfbakjdsbkjafbjksdfbakjsdfbkajsbdfakjsdbfkajbdfkajdsbfakjldbfakjdsfbakjdsfbjkdssbfksjdffbj"
+                    }
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 }
